b710623c099935f3a007dc276eafca7d5114493d
zyy <1755635168@qq.com>
2023-02-27 10:15:48 +0800

    version2.6

--- src/main/java/scs/controller/LoadGenController.java
+++ src/main/java/scs/controller/LoadGenController.java
@@ -102,12 +102,12 @@ public class LoadGenController {
 						{
 							ArrayList<Integer> timeList = new ArrayList<>();
 							int lastIndex = 0;
-							System.out.println("调用次数" + InvokeMap.get(i).size());
 							for(int j = 0;j < InvokeMap.get(i).size();j++)
 							{
 								if(InvokeMap.get(i).get(j) != 0)
 								{
 									Integer functionTime = j - lastIndex;
+									System.out.println(functionTime);
 									timeList.add(functionTime);
 									lastIndex = j;
 								}
