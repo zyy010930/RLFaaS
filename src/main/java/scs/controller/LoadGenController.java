b849ee8b9f63e2fc90ed1f6a9c4ddf22c126b218
zyy <1755635168@qq.com>
2023-04-26 10:27:50 +0800

    version13.3

--- src/main/java/scs/controller/LoadGenController.java
+++ src/main/java/scs/controller/LoadGenController.java
@@ -95,7 +95,7 @@ public class LoadGenController {
 
 						//Setting Memory Capacity
 						ConfigPara configPara = new ConfigPara();
-						configPara.setMemoryCapacity(45.0);
+						configPara.setMemoryCapacity(60.0);
 
 						System.out.println("start thread");
 
