d70a436b558ae74357cabbfc2ae97dc496322097
zyy <1755635168@qq.com>
2023-07-31 19:08:51 +0800

    version20.3

--- src/main/java/scs/controller/LoadGenController.java
+++ src/main/java/scs/controller/LoadGenController.java
@@ -342,7 +342,7 @@ public class LoadGenController {
 				throw new RuntimeException(e);
 			}
 			ArrayList<Double> list = new ArrayList<>();
-			for(int i = 1; i <= 720; i++)
+			for(int i = 1; i <= 240; i++)
 			{
 				try {
 					Thread.sleep(60000);
